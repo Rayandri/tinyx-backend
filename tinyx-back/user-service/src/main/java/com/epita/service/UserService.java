c104d41738380f6eac25e9bd37e1a558b94a853f
Alexis <alexis.petignat@epita.fr>
2025-03-12 17:53:57 +0000

    Fix: Forgot block repo

--- tinyx-back/user-service/src/main/java/com/epita/service/UserService.java
+++ tinyx-back/user-service/src/main/java/com/epita/service/UserService.java
@@ -1,4 +1,20 @@
-package main.java.com.epita.service;
+package com.epita.service;
 
+import com.epita.repository.BlockRepository;
+import com.epita.repository.FollowRepository;
+import com.epita.repository.LikeRepository;
+import com.epita.repository.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
 public class UserService {
+    @Inject
+    UserRepository userRepository;
+    @Inject
+    LikeRepository likeRepository;
+    @Inject
+    FollowRepository followRepository;
+    @Inject
+    BlockRepository blockRepository;
 }
