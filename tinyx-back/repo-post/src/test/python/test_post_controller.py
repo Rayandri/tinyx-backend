80f3cb86f80a0b8d90b9b1ec0476e78305c08787
Matthieu Hubschwerlin <matthieu.hubschwerlin@epita.fr>
2025-04-10 00:09:18 +0200

    function for test endpoint updated and working

--- tinyx-back/repo-post/src/test/python/test_post_controller.py
+++ tinyx-back/repo-post/src/test/python/test_post_controller.py
@@ -3,47 +3,47 @@ import unittest
 
 class TestPostController(unittest.TestCase):
 
-    BASE_URL = "http://localhost:8080/api/post"
+    BASE_URL = "http://localhost:8081/api/post"
 
-    def test_get_posts(self):
+#Example of uuid :
+# 113c3ceb-c562-4474-8066-427d0bffdb3b
+
+    def test_get_posts(self, status_code=200):
         response = requests.get(f"{self.BASE_URL}/all")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
         self.assertIsInstance(response.json(), list)
 
-    def test_get_user_posts(self):
-        user_id = "some-uuid"
+    def test_get_user_posts(self, user_id, status_code=200):
         response = requests.get(f"{self.BASE_URL}/user", params={"id": user_id})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
         self.assertIsInstance(response.json(), list)
 
-    def test_get_post(self):
-        post_id = "some-uuid"
+    def test_get_post(self, post_id, status_code=200):
         response = requests.get(f"{self.BASE_URL}", params={"id": post_id})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
         self.assertIsInstance(response.json(), dict)
 
-    def test_get_reply(self):
-        post_id = "some-uuid"
+    def test_get_reply(self, post_id, status_code=200):
         response = requests.get(f"{self.BASE_URL}/reply", params={"id": post_id})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
         self.assertIsInstance(response.json(), dict)
 
-    def test_add_post(self):
-        user_id = "some-uuid"
-        post_content = {
-            "content": "This is a test post",
-            "media": [],
-            "repost": None,
-            "replyTo": None
-        }
-        response = requests.post(f"{self.BASE_URL}", headers={"X-user-id": user_id}, json=post_content)
-        self.assertEqual(response.status_code, 200)
 
-    def test_delete_post(self):
-        user_id = "some-uuid"
-        post_id = "some-uuid"
+# Structure post_content :
+#post_content = {
+#    "content": "This is a test post",
+#    "media": [],
+#    "repost": None,
+#    "replyTo": None
+#}
+
+    def test_add_post(self, user_id, post_content, status_code=200):
+        response = requests.post(f"{self.BASE_URL}", headers={"X-user-id": user_id}, json=post_content)
+        self.assertEqual(response.status_code, status_code)
+
+    def test_delete_post(self, user_id, post_id, status_code=200):
         response = requests.delete(f"{self.BASE_URL}", headers={"X-user-id": user_id}, json={"id": post_id})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
 if __name__ == "__main__":
     unittest.main()
--- tinyx-back/repo-social/src/test/python/test_user_controller.py
+++ tinyx-back/repo-social/src/test/python/test_user_controller.py
@@ -4,85 +4,67 @@ import uuid
 
 class TestUserController(unittest.TestCase):
 
-    BASE_URL = "http://localhost:8080/api/user"
+    BASE_URL = "http://localhost:8082/api/user"
 
-    def test_get_likes(self):
+    def test_get_likes(self, status_code=200):
         response = requests.get(f"{self.BASE_URL}/likes/all")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_user_likes(self):
-        user_id = uuid.uuid4()
+    def test_get_user_likes(self, user_id=uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/likes", headers={"X-user-id": str(user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_user_likes_by_post(self):
-        post_id = uuid.uuid4()
+    def test_get_user_likes_by_post(self, post_id = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/likes/post", headers={"X-post-id": str(post_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_add_like(self):
-        user_id = uuid.uuid4()
-        post_id = uuid.uuid4()
+    def test_add_like(self, user_id = uuid.uuid4(), post_id = uuid.uuid4(), status_code=200):
         response = requests.post(f"{self.BASE_URL}/like", headers={"X-user-id": str(user_id), "X-post-id": str(post_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_unlike(self):
-        user_id = uuid.uuid4()
-        post_id = uuid.uuid4()
+    def test_unlike(self, user_id = uuid.uuid4(), post_id = uuid.uuid4(), status_code=200):
         response = requests.delete(f"{self.BASE_URL}/unlike", headers={"X-user-id": str(user_id), "X-post-id": str(post_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_follows(self):
+    def test_get_follows(self, status_code=200):
         response = requests.get(f"{self.BASE_URL}/follows/all")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_user_follows(self):
-        user_id = uuid.uuid4()
+    def test_get_user_follows(self, user_id = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/follows", headers={"X-user-id": str(user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_followers(self):
-        user_id = uuid.uuid4()
+    def test_get_followers(self, user_id = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/followers", headers={"X-user-id": str(user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_add_follow(self):
-        user_id = uuid.uuid4()
-        followed_user_id = uuid.uuid4()
+    def test_add_follow(self, user_id = uuid.uuid4(), followed_user_id = uuid.uuid4(), status_code=200):
         response = requests.post(f"{self.BASE_URL}/follow", headers={"X-user-id": str(user_id), "X-followed-id": str(followed_user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_unfollow(self):
-        user_id = uuid.uuid4()
-        followed_user_id = uuid.uuid4()
+    def test_unfollow(self, user_id = uuid.uuid4(), followed_user_id = uuid.uuid4(), status_code=200):
         response = requests.delete(f"{self.BASE_URL}/unfollow", headers={"X-user-id": str(user_id), "X-follow-id": str(followed_user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_blocks(self):
+    def test_get_blocks(self, status_code=200):
         response = requests.get(f"{self.BASE_URL}/blocks")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_user_blockers(self):
-        user_id = uuid.uuid4()
+    def test_get_user_blockers(self, user_id = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/blockers", headers={"X-user-id": str(user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_get_user_blocked(self):
-        user_id = uuid.uuid4()
+    def test_get_user_blocked(self, user_id = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/blocked", headers={"X-user-id": str(user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_add_block(self):
-        user_id = uuid.uuid4()
-        blocked_user_id = uuid.uuid4()
+    def test_add_block(self, user_id = uuid.uuid4(), blocked_user_id = uuid.uuid4(), status_code=200):
         response = requests.post(f"{self.BASE_URL}/block", headers={"X-user-id": str(user_id), "X-blocked-id": str(blocked_user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
-    def test_unblock(self):
-        user_id = uuid.uuid4()
-        blocked_user_id = uuid.uuid4()
+    def test_unblock(self, user_id = uuid.uuid4(), blocked_user_id = uuid.uuid4(), status_code=200):
         response = requests.delete(f"{self.BASE_URL}/unblock", headers={"X-user-id": str(user_id), "X-blocked-id": str(blocked_user_id)})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status_code)
 
 if __name__ == "__main__":
     unittest.main()
