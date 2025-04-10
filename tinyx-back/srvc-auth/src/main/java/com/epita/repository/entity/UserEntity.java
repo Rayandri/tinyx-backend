dbe511e5029591123d53b055ad0ba8294f831d34
RayanDri <rayan.drissi@icloud.com>
2025-04-10 12:30:20 +0200

    refactor UserEntity: simplify constructor and use @Data annotation for boilerplate code

--- tinyx-back/srvc-auth/src/main/java/com/epita/repository/entity/UserEntity.java
+++ tinyx-back/srvc-auth/src/main/java/com/epita/repository/entity/UserEntity.java
@@ -1,33 +1,25 @@
 package com.epita.repository.entity;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.codecs.pojo.annotations.BsonId;
-
-
 
 import java.util.Date;
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @MongoEntity(collection = "Users")
 public class UserEntity {
 
     @BsonId
-    private final UUID id;
+    private UUID id = UUID.randomUUID();
     public String username;
     public String password_hash;
     public Date created_at;
     public Date updated_at;
 
-
-    public UserEntity(String name,String password_hash, Date created_at, Date updated_at) {
-        this.username = name;
-        this.id = UUID.randomUUID();
-        this.password_hash = password_hash;
-        this.updated_at = updated_at;
-        this.created_at = created_at;
-    }
 }
