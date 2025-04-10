023308d858bd2a7ef2cb1a2ae3a04ecbaacfe937
Matthieu Hubschwerlin <matthieu.hubschwerlin@epita.fr>
2025-04-10 11:45:47 +0200

    add response to tests functions

--- tinyx-back/tests/test_search_controller.py
+++ tinyx-back/tests/test_search_controller.py
@@ -9,14 +9,17 @@ class TestSearchController(unittest.TestCase):
     def test_search_post(self, words: str="test", hashtags: str="#test", status_code=200):
         response = requests.get(f"{self.BASE_URL}/posts", params={"words": words, "hashtags": hashtags})
         self.assertEqual(status_code, response.status_code)
+        return response
 
     def test_get_user_posts(self, user_id: uuid = uuid.uuid4(), status_code=200):
         response = requests.get(f"{self.BASE_URL}/posts/{user_id}")
         self.assertEqual(status_code, response.status_code)
+        return response
 
     def test_get_user_name(self, name: str = "name", status_code=200):
         response = requests.get(f"{self.BASE_URL}/posts/{name}")
         self.assertEqual(status_code, response.status_code)
+        return response
 
 if __name__ == "__main__":
     unittest.main()
